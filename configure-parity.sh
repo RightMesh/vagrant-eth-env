3cb9c6ab30623c18a71d226e7b64b9433b208d4b
Dr. Jason Ernst <jason@left.io>
2018-01-17 13:37:41 -0800

    Updated for kovan test network
    Previously this was using a private chain - this update switches it to the Kovan testnet.

--- configure-parity.sh
+++ configure-parity.sh
@@ -35,45 +35,4 @@ echo $PASSWORD >> .parity-pass
 address="133e5245e3e5ab3f65e73120b34cc29f0f7ba504"
 echo "address: $address"
 
-cat > chain.json <<EOL
-{
-  "name": "Private",
-  "engine": {
-    "InstantSeal": null
-  },
-  "params": {
-    "accountStartNonce": "0x00",
-    "maximumExtraDataSize": "0x20",
-    "minGasLimit": "0x1388",
-    "networkID" : "0xad"
-  },
-  "genesis": {
-    "seal": {
-      "ethereum": {
-        "nonce": "0x00006d6f7264656e",
-        "mixHash": "0x00000000000000000000000000000000000000647572616c65787365646c6578"
-      }
-    },
-    "difficulty": "0x20000",
-    "author": "0x0000000000000000000000000000000000000000",
-    "timestamp": "0x00",
-    "parentHash": "0x0000000000000000000000000000000000000000000000000000000000000000",
-    "extraData": "0x",
-    "gasLimit": "0x1312d00"
-  },
-  "nodes": [
-  ],
-  "accounts": {
-    "0000000000000000000000000000000000000001": { "balance": "1", "nonce": "1048576", "builtin": { "name": "ecrecover", "pricing": { "linear": { "base": 3000, "word": 0 } } } },
-    "0000000000000000000000000000000000000002": { "balance": "1", "nonce": "1048576", "builtin": { "name": "sha256", "pricing": { "linear": { "base": 60, "word": 12 } } } },
-    "0000000000000000000000000000000000000003": { "balance": "1", "nonce": "1048576", "builtin": { "name": "ripemd160", "pricing": { "linear": { "base": 600, "word": 120 } } } },
-    "0000000000000000000000000000000000000004": { "balance": "1", "nonce": "1048576", "builtin": { "name": "identity", "pricing": { "linear": { "base": 15, "word": 3 } } } },
-    "${address}": {
-      "balance": "1606938044258990275541962092341162602522202993782792835301376"
-    }
-  }
-}
-EOL
-
-parity --chain chain.json --author ${address} --unlock ${address} --password .parity-pass --rpccorsdomain "*" --jsonrpc-interface all --jsonrpc-hosts all --no-dapps --no-signer >&1 1>>parity.log 2>&1 &
-
+parity --geth --chain kovan --force-ui --reseal-min-period 0 --jsonrpc-cors "*" --jsonrpc-apis web3,eth,net,parity,traces,rpc,personal --author ${address} --unlock ${address} --password .parity-pass
